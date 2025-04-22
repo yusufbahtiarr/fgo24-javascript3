bc88861d900dd7f84448b13bc37f9d09b211a181
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-22 11:53:36 +0700

    Update minitask6.js

--- minitask6.js
+++ minitask6.js
@@ -1,20 +1,26 @@
 const url = "https://jsonplaceholder.typicode.com/users"
 
+// then-catch
 fetch(url)
 .then(res=>{
     return res.json()    
 }).then(data=>{
-    result = []
-    lower = ""
-    data.forEach(item => {
-        lower = item.email
-        result.push(lower.toLowerCase())
-    })
-    console.log(result);
-    
-
+    console.log(data.map(item => item.email.toLowerCase()))
 })
 .catch(err => {
-    console.log("Parsing data gagal");
-    
+    console.log("Parsing data gagal");  
 })
+
+//async-await
+const retrieveData = async()=>{
+    try{ //jika ada gagal bisa gunakan try-catch
+        const fetchData = await fetch(url)
+        const data = await fetchData.json()
+        // console.log(data) // test tampilkan data
+        const emailUsers = data.map(item=>item.email.toLowerCase())
+        console.log(emailUsers);
+    } catch(err) {
+        console.log(err);
+    }
+}
+retrieveData()
